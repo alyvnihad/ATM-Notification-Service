3b98a3de97d54b733b2f2cba36595ac1630c05e0
alyvnihad <aliyevnihad011@gmail.com>
2025-10-23 11:49:51 +0400

    MailController update.v1 controller class

--- src/main/java/org/example/notificationservice/controller/MailController.java
+++ src/main/java/org/example/notificationservice/controller/MailController.java
@@ -2,6 +2,7 @@ package org.example.notificationservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.notificationservice.payload.MailPayload;
+import org.example.notificationservice.payload.TransactionPayload;
 import org.example.notificationservice.service.MailService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,5 +19,10 @@ public class MailController {
     @PostMapping("/register-send")
     public void sendEmailPdf(@RequestBody MailPayload payload){
         mailService.sendEmailPdf(payload);
+    }
+
+    @PostMapping("/email-log")
+    public void sendEmailLog(@RequestBody TransactionPayload payload){
+        mailService.sendEmailLog(payload);
     }
 }
